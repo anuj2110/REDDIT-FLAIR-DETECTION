f126627fb4a566aeadca8ab8d2d162569864c5f3
anuj2110 <atrehan789@gmail.com>
2020-04-21 18:21:33 +0530

    Changed the strategy for data collection,analysis

--- redditflairdetector/flairdetector/views.py
+++ redditflairdetector/flairdetector/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def predictOnLink(request):
+    return render(request, 'index.html')
+
+def automatedTesting(request):
+    return render(request,'automated_testing.html')
