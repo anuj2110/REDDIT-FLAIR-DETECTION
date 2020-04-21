409cccc76418efd37a5a04b2a287d0105b168d1f
anuj2110 <atrehan789@gmail.com>
2020-04-21 23:39:02 +0530

    Added frontend and changed code of model training

--- redditflairdetector/flairdetector/views.py
+++ redditflairdetector/flairdetector/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
 
 def predictOnLink(request):
-    return render(request, 'index.html')
+    if request.method=='POST':
+        string = str(request.POST.get('URL'))
+        if string is not None:
+            url = {'url': string,'show':True}
+            return render(request,'index.html',url)
+        else:
+            return render(request,'index.html',{'show':False})
+    return render(request, 'index.html',{'show':False})
 
 def automatedTesting(request):
     return render(request,'automated_testing.html')
