9e6be2debeb8551b0f8d06f326d3dea7577e13ec
anuj2110 <atrehan789@gmail.com>
2020-04-25 13:06:34 +0530

    Added resources for heroku

--- webapp/settings.py
+++ webapp/settings.py
@@ -124,3 +124,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
