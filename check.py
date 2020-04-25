34b5b2a3ccb66b2eeac530233b7c2a5d8b6b51d3
anuj2110 <atrehan789@gmail.com>
2020-04-25 15:22:32 +0530

    some minor changes

--- check.py
+++ check.py
@@ -1,5 +1,5 @@
 import requests
 files = {'upload_file': open('file.txt','rb')}
 
-r = requests.post('http://127.0.0.1:8000/automated_testing', files=files)
+r = requests.post('https://reddit-flair-ind-det-anj.herokuapp.com/automated_testing', files=files)
 print(r.json())
