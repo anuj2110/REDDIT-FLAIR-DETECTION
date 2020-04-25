1f459189466850c2aa8193616425a929c4523d63
anuj2110 <atrehan789@gmail.com>
2020-04-25 16:49:59 +0530

    Bug fixes

--- reddit_flair_detector/views.py
+++ reddit_flair_detector/views.py
@@ -11,24 +11,27 @@ from django.conf import settings
 def predictOnLink(request):
     if request.method=='POST':
         url = str(request.POST.get('URL'))
-        if url is not None:
+        if url!="" and 'https://www.reddit.com/r/india/comments/' in url and url!='https://www.reddit.com/r/india/comments/':
             pred = makePredictions(url,ml_model,preprocessor,reddit,stopwords,labeltoflair)
             return render(request,'index.html',{'url':url,'show':True,'pred':pred})
         else:
-            return render(request,'index.html',{'show':False})
+            return render(request,'index.html',{'show':False,'error':'please enter a valid url'})
     return render(request, 'index.html',{'show':False})
 @csrf_exempt
 def automatedTesting(request):
     if request.method=='POST':
-        myfile = request.FILES['upload_file']
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name, myfile)
-        # myfile = open(os.path.join(settings.MEDIA_ROOT, filename), 'rb')
-        url_dict={}
-        with open(os.path.join(settings.MEDIA_ROOT, filename), 'r') as var:
-            for url in var:
-                url = url.rstrip()
-                url_dict[url]= makePredictions(url,ml_model,preprocessor,reddit,stopwords,labeltoflair)
-        os.remove(path=os.path.join(settings.MEDIA_ROOT, filename))
-        return JsonResponse(url_dict)
+        try:
+            myfile = request.FILES['upload_file']
+            fs = FileSystemStorage()
+            filename = fs.save(myfile.name, myfile)
+            url_dict={}
+            with open(os.path.join(settings.MEDIA_ROOT, filename), 'r') as var:
+                for url in var:
+                    url = url.rstrip()
+                    url_dict[url]= makePredictions(url,ml_model,preprocessor,reddit,stopwords,labeltoflair)
+                os.remove(path=os.path.join(settings.MEDIA_ROOT, filename))
+            return JsonResponse(url_dict)
+        except:
+            return render(request,'automated_testing.html',{'error':"Please add a file"})
+        
     return render(request,'automated_testing.html')
